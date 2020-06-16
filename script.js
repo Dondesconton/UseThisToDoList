fecd33c35326855b804b5e223099f66d1f82c9ab
Andrés <45668836+Dondesconton@users.noreply.github.com>
2020-06-16 17:56:55 -0500

    Finished  DOM coding challenge
    This list allows you to add and delete items to your shopping list.

---
+++ script.js
@@ -0,0 +1,52 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+
+
+
+function inputLength() {
+	return input.value.length;
+}
+//Creates Li and button items  and Removes the whole thing if the callback function is triggered.
+function createListElement() {
+	var li = document.createElement("li");//Crea el tagElement
+	li.appendChild(document.createTextNode(input.value));//crea el texto del tagElement from the input value.
+	ul.appendChild(li);
+	input.value = "";
+
+	var deleteBtn = document.createElement('button');
+	deleteBtn.appendChild(document.createTextNode("Delete"));
+	li.appendChild(deleteBtn);
+	deleteBtn.onclick = removeParent;
+}
+//Ads item to the ul
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
+
+//creates toggle event for a tageted clicked
+
+ul.onclick = function(event){
+	var target = event.target;
+	target.classList.toggle('done');
+}
+
+
+//Remove a list targeted <li> element
+function removeParent(event){
+	event.target.parentElement.remove();
+	
+}
