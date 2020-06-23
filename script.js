690ba829a12f74222d0e6c9ae9a57b02d8d70eab
Andrés <45668836+Dondesconton@users.noreply.github.com>
2020-06-23 02:48:46 -0500

    Commented my arrow function

--- script.js
+++ script.js
@@ -22,13 +22,13 @@ function createListElement() {
 }
 //Ads item to the ul
 
-/*function addListAfterClick() {
+function addListAfterClick() {
 	if (inputLength() > 0) {
 		createListElement();
 	}
-}*/
+}
 
-const addListAfterClick =()=>inputLenght()>0 ? createListElement():undefined;
+/*const addListAfterClick =()=>inputLenght()>0 ? createListElement():undefined;*/
 
 function addListAfterKeypress(event) {
 	if (inputLength() > 0 && event.keyCode === 13) {
